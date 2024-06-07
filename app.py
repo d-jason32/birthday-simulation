8a21fbb056d9261f5c1a14a5b9e5d5594f02950a
Jason Devaraj <jdevaraj01@gmail.com>
2024-06-07 19:48:19 -0400

    Added progress bar

--- app.py
+++ app.py
@@ -1,53 +1,25 @@
 import random
 import datetime
+from time import sleep
+from tqdm import tqdm
 
 def comparison(birthdayList):
-    sameBirthday = 0
-    hashset = set()
-    for i in range(len(birthdayList)):
-        if birthdayList[i] in hashset:
-            sameBirthday += 1
-        hashset.add(birthdayList[i])
-    return sameBirthday
-
-def simulation_count(simulationNumber):
-    if simulationNumber == 10_000:
-        print("10,000 simulations run...")
-    elif simulationNumber == 20_000:
-        print("20,000 simulations run...")
-    elif simulationNumber == 30_000:
-        print("30,000 simulations run...")
-    elif simulationNumber == 40_000:
-        print("40,000 simulations run...")
-    elif simulationNumber == 50_000:
-        print("50,000 simulations run...")
-    elif simulationNumber == 60_000:
-        print("60,000 simulations run...")
-    elif simulationNumber == 70_000:
-        print("70,000 simulations run...")
-    elif simulationNumber == 80_000:
-        print("80,000 simulations run...")
-    elif simulationNumber == 90_000:
-        print("90,000 simulations run...")
-    elif simulationNumber == 99_999:
-        print("100,000 simulations run")
+    return len(birthdayList) - len(set(birthdayList))
 
 def simulation(numBirthdays):
     numSameBirthdays = 0
-    list = []
     print(f"\n\nGenerating {numBirthdays} random birthdays 100,000 times...")
-    foo = input("Press Enter to continue...")
-    for i in range(100_000):
-        simulation_count(i)
-        list = generateBirthdays(numBirthdays)
-        numSameBirthdays += comparison(list)
-    print(f"Out of 100,000 simulations of {numBirthdays} people, there was a "
+    input("Press Enter to continue...")
+    for i in tqdm(range(1_000_001)):
+        # simulation_count(i)
+        birthdayList = generateBirthdays(numBirthdays)
+        numSameBirthdays += comparison(birthdayList)
+    percentage = numSameBirthdays / 10000
+    print(f"Out of 1,000,000 simulations of {numBirthdays} people, there was a "
       f"matching birthday in the group {numSameBirthdays} times. This means "
-      f"that {numBirthdays} people have a {numSameBirthdays / 1000:.2f}% chance of "
+      f"that {numBirthdays} people have a {percentage:.2f}% chance of "
       f"having a matching birthday in their group. "
       "That's probably more than you would think!")
-
-
 
 def firstComparison(birthdayList):
     hashset = set()
@@ -60,7 +32,7 @@ def generateBirthdays(birthdaysRequested):
     startDate = datetime.datetime(1980,1,1)
     birthdayList = []
     for i in range(birthdaysRequested):
-        randomDay = random.randrange(366)
+        randomDay = random.randrange(365)
         newDate = startDate + datetime.timedelta(days=randomDay)
         birthdayList.append(newDate.strftime("%b %d"))
     return birthdayList
